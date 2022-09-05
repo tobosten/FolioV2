5fe130aeeab2ed4dac95662ef9eef5a531af74df
tobosten <tobbe.osterlin@gmail.com>
2022-09-05 16:59:48 +0200

    Implemented BrowserRouter.

---
+++ src/Home.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import jpnImage from "./assets/jpnbgimg.jpg"
+import { Link } from 'react-router-dom'
+
+function Home() {
+    return (
+        <div className="App">
+            <div
+                className='backgroundImage'
+                style={{
+                    backgroundImage: `url(${jpnImage})`,
+                }}
+            >
+                <div className='backdropDiv'>
+                    <div className='backdropDivText'>Hello!<br />
+                        My name is Tobias
+                    </div>
+                </div>
+
+
+                <div className='mainProjectsContainer'>
+                    <div className='projectsTitle'>Projects</div>
+                    <div className='projectsContainer'>
+
+                        <Link to="/actionreminder" className='project1' >
+                            
+                        </Link> 
+                        <div className='project'>
+
+                        </div>
+                        <div className='project'>
+
+                        </div>
+                        <div className='project'>
+
+                        </div>
+                        <div className='project'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Projects/ActionReminder/ActionReminder.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function ActionReminder() {
+  return (
+    <div>ActionReminder</div>
+  )
+}
+
+export default ActionReminder
